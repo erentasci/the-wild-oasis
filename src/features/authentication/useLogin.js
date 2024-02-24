aeaf1af48919ff93e71f4f3340dc3bd51c1fe6d4
Eren Tasci <erent4sci@gmail.com>
2024-02-24 14:50:09 +0300

    feat: adding authenticated and authorization

---
+++ src/features/authentication/useLogin.js
@@ -0,0 +1,24 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import toast from "react-hot-toast";
+import { login as loginApi } from "../../services/apiAuth";
+import { useNavigate } from "react-router-dom";
+
+export function useLogin() {
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
+  const { mutate: login, isLoading } = useMutation({
+    mutationFn: ({ email, password }) => loginApi({ email, password }),
+    onSuccess: (user) => {
+      console.log("User: ", user);
+      queryClient.setQueriesData(["user"], user);
+      toast.success("Login successfully");
+      navigate("/dashboard");
+    },
+    onError: (error) => {
+      console.log("ERROR: ", error.message);
+      toast.error("Provided email or password is incorrect. Please try again.");
+    },
+  });
+
+  return { login, isLoading };
+}
